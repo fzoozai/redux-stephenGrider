c1fab8c5879d0048fdd2486954ed1f8f69e988c9
fzoozai <fabian.soosaithasan@gmail.com>
2017-04-30 11:22:45 +0200

    Clean up structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
+
 const API_KEY = 'AIzaSyD8useUf94jN0MgXPAWVGUXM36MunsnFtw';
 
 
